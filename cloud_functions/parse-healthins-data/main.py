655159e5b728084c2d5343735820211d819ce97d
José Nunes <josenunesra@gmail.com>
2021-01-08 22:13:57 -0300

    refactoring saving parquet file to storage

--- cloud_functions/parse-healthins-data/main.py
+++ cloud_functions/parse-healthins-data/main.py
@@ -1,7 +1,10 @@
-import pandas as pd
-import dask.dataframe as dd
+import json
+import base64
 from uuid import uuid4
 from datetime import datetime
+
+import pandas as pd
+import dask.dataframe as dd
 
 
 def main(event, context):
@@ -11,7 +14,7 @@ def main(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    pubsub_message = event['data']
+    pubsub_message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
 
     pandas_df = pd.DataFrame(pubsub_message['rows'], columns=pubsub_message['columns'])
 
@@ -21,31 +24,4 @@ def main(event, context):
     now = datetime.utcnow()
     path = f"gs://healthins-parsed-data/{now.year}/{'{:02d}'.format(now.month)}/{'{:02d}'.format(now.day)}/{'{:02d}'.format(now.hour)}/{uuid4().hex}"
     dask_df.to_parquet(path,
-                       storage_options={'token': '/home/jose/Projects/PycharmProjects/etus/key.json'})
-
-
-    # client = storage.Client.from_service_account_json('/home/jose/Projects/PycharmProjects/etus/key.json')
-    # bucket = client.get_bucket('healthins-parsed-data')
-    #
-    # now = datetime.utcnow()
-    #
-    # blob2 = bucket.blob(
-    #     f"{now.year}/{'{:02d}'.format(now.month)}/{'{:02d}'.format(now.day)}/{'{:02d}'.format(now.hour)}/{now.strftime('%Y%m%d%H%M%S')}_{uuid4().hex}.json")
-    # blob2.upload_from_string(pubsub_message, content_type='application/json')
-
-
-# if __name__ == '__main__':
-#     pandas_df = pd.DataFrame(message_example['rows'], columns=message_example['columns'])
-#
-#     dask_df = dd.from_pandas(pandas_df, npartitions=1)
-#     dask_df = dask_df.reset_index(drop=True)
-#
-#     now = datetime.utcnow()
-#     path = f"gs://healthins-parsed-data/{now.year}/{'{:02d}'.format(now.month)}/{'{:02d}'.format(now.day)}/{'{:02d}'.format(now.hour)}"
-#     dask_df.to_parquet(path,
-#                        storage_options={'token': '/home/jose/Projects/PycharmProjects/etus/key.json'})  # use a service account key with correct permissions
-#
-#     df = dd.read_parquet(path,
-#                          storage_options={'token': '/home/jose/Projects/PycharmProjects/etus/key.json'})
-#
-#     print(df.head())
+                       storage_options={'token': './key.json'})
